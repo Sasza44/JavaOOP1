4252fb6f88a06d74500aa8f0aceb42402bcfcb85
Sasza44 <110614574+Sasza44@users.noreply.github.com>
2023-07-20 21:33:36 +0300

    Add files via upload

--- Homework1/src/second/level/Phone.java
+++ Homework1/src/second/level/Phone.java
@@ -25,20 +25,8 @@ public class Phone {
 			i += 1;
 		}
 		numbers[i] = n;
-		//System.out.println(this.number);
 	}
 	public void outgoingCall(Phone p) { // вихідний дзвінок
-//		int i = 0;
-//		boolean b1 = false; // біт, який виявляє, чи зареєстрований номер абонента в мережі
-//		boolean b2 = false; // біт, який виявляє, чи зареєстрований номер, на який дзвонить абонент, в мережі
-//		while(numbers[i] != null) {
-//			if(numbers[i].getNumber() == this.number) {b1 = true;}
-//			else if(numbers[i].getNumber() == p.getNumber()) {b2 = true;}
-//			i += 1;
-//		}
-//		if(b1 == false) {System.out.println("Your number isn't registered");}
-//		else if(b2 == false) {System.out.println("Subscriber's number isn't registered");}
-//		else {System.out.println("Your call is successful");}
 		String s1 = this.number.substring(0, 3); // перші 3 символи цього номеу
 		String s2 = p.getNumber().substring(0, 3); // перші 3 символи номеру, на який здійснюється дзвінок
 		if(!s1.equals("+38")) {
