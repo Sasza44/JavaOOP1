4b2e19afa440d57867982b2b0fd05f9729b5ad58
Sasza44 <110614574+Sasza44@users.noreply.github.com>
2023-07-20 21:27:47 +0300

    Add files via upload

--- Homework1/src/second/level/First.java
+++ Homework1/src/second/level/First.java
@@ -8,7 +8,7 @@ public class First {
 		Network[] numbers = new Network[10]; // масив зареєстрованих номерів (з запасом)
 		phone1.register(numbers); // реєстрація 1-го номера в мережі оператора
 		phone2.register(numbers); // реєстрація 2-го номера в мережі оператора
-		phone1.outgoingCall("097-983-90-60", numbers); // вихідний дзвінок з 1-го номера на 2-ий
-		phone1.inletCall("097-983-90-60"); // вихідний дзвінок на 1-й номер з 2-го
+		phone1.outgoingCall(phone2); // вихідний дзвінок з 1-го номера на 2-ий
+		phone1.inletCall(phone2); // вихідний дзвінок на 1-й номер з 2-го
 	}
 }
--- Homework1/src/second/level/Phone.java
+++ Homework1/src/second/level/Phone.java
@@ -16,29 +16,42 @@ public class Phone {
 	public String toString() {
 		return "Phone [number=" + number + "]";
 	}
-	
+
 	public void register(Network[] numbers) { // реєстрація номеру телефону в мережі
+		this.number = "+38-" + this.number;
 		Network n = new Network(this.number);
 		int i = 0;
 		while(numbers[i] != null) {
 			i += 1;
 		}
 		numbers[i] = n;
+		//System.out.println(this.number);
 	}
-	public void outgoingCall(String number, Network[] numbers) { // вихідний дзвінок
-		int i = 0;
-		boolean b1 = false; // біт, який виявляє, чи зареєстрований номер абонента в мережі
-		boolean b2 = false; // біт, який виявляє, чи зареєстрований номер, на який дзвонить абонент, в мережі
-		while(numbers[i] != null) {
-			if(numbers[i].getNumber() == this.number) {b1 = true;}
-			else if(numbers[i].getNumber() == number) {b2 = true;}
-			i += 1;
+	public void outgoingCall(Phone p) { // вихідний дзвінок
+//		int i = 0;
+//		boolean b1 = false; // біт, який виявляє, чи зареєстрований номер абонента в мережі
+//		boolean b2 = false; // біт, який виявляє, чи зареєстрований номер, на який дзвонить абонент, в мережі
+//		while(numbers[i] != null) {
+//			if(numbers[i].getNumber() == this.number) {b1 = true;}
+//			else if(numbers[i].getNumber() == p.getNumber()) {b2 = true;}
+//			i += 1;
+//		}
+//		if(b1 == false) {System.out.println("Your number isn't registered");}
+//		else if(b2 == false) {System.out.println("Subscriber's number isn't registered");}
+//		else {System.out.println("Your call is successful");}
+		String s1 = this.number.substring(0, 3); // перші 3 символи цього номеу
+		String s2 = p.getNumber().substring(0, 3); // перші 3 символи номеру, на який здійснюється дзвінок
+		if(!s1.equals("+38")) {
+			System.out.println("Your number isn't registered");
 		}
-		if(b1 == false) {System.out.println("Your number isn't registered");}
-		else if(b2 == false) {System.out.println("Subscriber's number isn't registered");}
-		else {System.out.println("Your call is successful");}
+		if(!s2.equals("+38")) {
+			System.out.println("Subscriber's number isn't registered");
+		}
+		if(s1.equals("+38") && s2.equals("+38")) {
+			System.out.println("Your call is successful");
+		}
 	}
-	public void inletCall(String number) { // вхідний дзвінок
-		System.out.println("Number " + number + " is calling you");
+	public void inletCall(Phone p) { // вхідний дзвінок
+		System.out.println("Number " + p.getNumber() + " is calling you");
 	}
 }
